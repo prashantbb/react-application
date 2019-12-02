56d12028a63dcadd3400bd9aad3d884db3900e41
prashantbb <pbedekar@rediffmai.comm>
2019-12-02 17:46:39 +0530

    Updates

---
+++ precache-manifest.e17f114458b2c98ccd79f3cb2a318154.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6ff0744aca5f2bf60c8426107df38f7",
+    "url": "/react-application.git/index.html"
+  },
+  {
+    "revision": "896a766e620c89b7f38a",
+    "url": "/react-application.git/static/css/main.f6083c5a.chunk.css"
+  },
+  {
+    "revision": "fb07f8efcf534988d5d6",
+    "url": "/react-application.git/static/js/2.7a8e760c.chunk.js"
+  },
+  {
+    "revision": "896a766e620c89b7f38a",
+    "url": "/react-application.git/static/js/main.6d76ca46.chunk.js"
+  },
+  {
+    "revision": "a0f6c871992f06c07a57",
+    "url": "/react-application.git/static/js/runtime-main.133efdfd.js"
+  }
+]);
